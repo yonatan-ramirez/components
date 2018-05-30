19782460715a7c6f7dca2d7090f13f0797441e3f
yonatan-ramirez <yonatan.ramirez557@gmail.com>
2018-05-30 16:59:20 -0700

    components

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,12 @@ import { Component } from "@angular/core";
     templateUrl: './server.component.html'
 })
 export class ServerComponent{
+    serverId: number = 10;
+    serverStatus: string = 'offline';
+    flag: boolean = false;
+    
+    getServerStatus(){
+        return this.serverStatus;
+    }
 
 }
